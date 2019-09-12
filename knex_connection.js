47bd7ed53b211de4a6e7b9a873a88a54335ab065
RobertDruska <robert.druska@gmail.com>
2019-09-13 01:47:19 +0200

    fix

--- knex_connection.js
+++ knex_connection.js
@@ -1,39 +1,37 @@
 // development
-const connection = require('knex')({
-    client: 'mysql',
-    connection: {
-      host     : 'localhost',
-      user     : 'root',
-      password : '',
-      database : 'sudoku',
-      port     : '3306',
-      charset  : 'utf8'
-    },
-    pool: {
-      min: 0,
-      max: 20,
-    },
-  });
+// const connection = require('knex')({
+//     client: 'mysql',
+//     connection: {
+//       host     : 'localhost',
+//       user     : 'root',
+//       password : '',
+//       database : 'sudoku',
+//       port     : '3306',
+//       charset  : 'utf8'
+//     },
+//     pool: {
+//       min: 0,
+//       max: 20,
+//     },
+//   });
 
 // production ...
-// const connection = require('knex')({
-//   production: {
-//     client: 'postgresql',
-// 		connection: {
-//       host: 'ec2-174-129-226-232.compute-1.amazonaws.com',
-//       database: 'd24kkncnv2h51',
-//       user: 'gpucdqerafrfkt',
-//       password: '2dd85436bcb489b342251407ba8cf04c1fe26ecf3734d3e2ec9262e0e0277aa3',
-//       port: 5432
-// 		},
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   }
-// })
+const connection = require('knex')({
+    client: 'postgresql',
+		connection: {
+      host: 'ec2-174-129-226-232.compute-1.amazonaws.com',
+      database: 'd24kkncnv2h51',
+      user: 'gpucdqerafrfkt',
+      password: '2dd85436bcb489b342251407ba8cf04c1fe26ecf3734d3e2ec9262e0e0277aa3',
+      port: 5432
+		},
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+});
 
 module.exports = connection;
